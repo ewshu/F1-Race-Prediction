b787afa2cb19ce05cdf4b3b9fdfcf47528d6a1fe
Eshwar <pamula.3@osu.edu>
2024-11-28 21:45:05 -0500

    Update multiple files with new changes

--- src/models/app.py
+++ src/models/app.py
@@ -1,11 +1,10 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import joblib
 import os
-import plotly.graph_objects as go
-import plotly.express as px
 from datetime import datetime
+import joblib
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
 
 
 class F1WebPredictor:
